5a4f7e476696c667e53fba8f26e895c6211018f3
manmohan <manmohansharma987@gmail.com>
2014-03-03 20:39:31 +0530

    This is updating using PUT

--- webassigment.py
+++ webassigment.py
@@ -15,21 +15,32 @@ def add_patient():
     p_dict.update({p_id:store_info})
     return p_dict
 
-@get('/patient_show')
-def show_patient():
-    p_id = request.POST['id']
+@get('/patient_show/<p_id>')
+def show_patient(p_id):
     if p_id not in p_dict.keys():
         return 'This is not a correct patient id'
     else:
         return p_dict[p_id]
 
-@delete('/patient_delete')
-def delete_patient():
+
+@put('/patient_update')
+def patient_update():
     p_id = request.POST['id']
+    p_name = request.POST['name']
+    p_gender = request.POST['gender']
+    p_age = request.POST['age']
+    p_address = request.POST['address']
+    pa_phone = request.POST['phone']
+    store_info = ' '.join([p_name,p_gender,p_age,p_address,pa_phone])
+    p_dict.update({p_id:store_info})
+    return p_dict
+
+@delete('/patient_delete/<p_id>')
+def delete_patient(p_id):
     if p_id not in p_dict.keys():
         return 'This is not a correct patient id'
     else:
-		p_dict.pop(p_id)
-		return p_dict
+	p_dict.pop(p_id)
+	return p_dict
 
 run(host='localhost',port=8080)
